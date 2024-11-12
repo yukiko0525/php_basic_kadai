cb03f20ed7c7dec712a274b64f06e260c2b5fbfb
yukiko0525 <pianox2xwxwx5@gmail.com>
2024-11-12 20:52:12 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>PHP課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+
+    $vegetable = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($vegetable);
+
+    ?>
+
+  </p>
+</body>
+
+</html>
